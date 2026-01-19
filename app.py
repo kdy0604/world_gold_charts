f83a9c829a581dc959b7bd78bf0b1efdc47db550
kdy0604 <kimdy0604@naver.com>
2026-01-19 12:55:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ st.set_page_config(page_title="금 시세", layout="centered")
 
 # 2. CSS를 이용한 글자 크기 및 여백 미세 조정
 
+# st.markdown("""
+#     <style>
+#     .main-title { font-size: 24px !important; font-weight: 700; margin-bottom: 10px; }
+#    .sub-title { font-size: 16px !important; color: #666; }
+#    .price-text { font-size: 25px !important; font-weight: 800; color: #1E1E1E;} #  margin-bottom: -5px;  삭제
+#    .exchange-text { font-size: 18px !important; font-weight: 600; color: #444; }
+#    .stMetric { padding: 0px !important; }
+#    </style>
+#    """, unsafe_allow_html=True)
+
 st.markdown("""
     <style>
     /* 가로 정렬을 강제하는 컨테이너 */
@@ -52,42 +62,8 @@ try:
         """, unsafe_allow_html=True
     )
 
-    .main-title { font-size: 24px !important; font-weight: 700; margin-bottom: 10px; }
-    .sub-title { font-size: 16px !important; color: #666; }
-    .price-text { font-size: 25px !important; font-weight: 800; color: #1E1E1E;} #  margin-bottom: -5px;  삭제
-    .exchange-text { font-size: 18px !important; font-weight: 600; color: #444; }
-    .stMetric { padding: 0px !important; }
-            
-except Exception as e:
-    st.error("데이터 로딩 중...")
-
-@st.cache_data(ttl=1800)
-def get_gold_data():
-    gold = yf.Ticker("GC=F").history(period="1mo")
-    exchange = yf.Ticker("KRW=X").history(period="1mo")
-    df = pd.DataFrame({'gold_usd': gold['Close'], 'usd_krw': exchange['Close']}).ffill()
-    df['price_krw_don'] = (df['gold_usd'] * df['usd_krw']) / 31.1035 * 3.75
-    return df
-    
-try:
-    data = get_gold_data()
-    current_price = data['price_krw_don'].iloc[-1]
-    current_ex = data['usd_krw'].iloc[-1]
-    last_gold_usd = data['gold_usd'].iloc[-1]
-    
-    # 상단 텍스트 (크기 조절됨)
-    st.markdown('<p class="main-title">💰 실시간 금 1돈 국제 시세</p>', unsafe_allow_html=True)
-    
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown('<p class="sub-title">금 1돈 (3.75g)</p>', unsafe_allow_html=True)
-        st.markdown(f'<p class="price-text">{int(current_price):,} 원</p>', unsafe_allow_html=True)
-    with col2:
-        st.markdown('<p class="sub-title">현재 환율</p>', unsafe_allow_html=True)
-        st.markdown(f'<p class="exchange-text">{current_ex:.2f} 원/$</p>', unsafe_allow_html=True)
-
     st.write(f"🌐 국제 시세: ${last_gold_usd:.2f} / t oz")
-
+    
     # --- 차트 설정 (모바일에서 보기 좋게 여백 제거) ---
     y_min, y_max = data['price_krw_don'].min() * 0.995, data['price_krw_don'].max() * 1.005
     fig = px.line(data, y='price_krw_don')
@@ -100,6 +76,12 @@ try:
         dragmode=False,  # 차트 위에서 드래그(슬라이딩) 기능을 끔
     )
     st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+            
+except Exception as e:
+    st.error("데이터 로딩 중...")
+
+# @st.cache_data(ttl=1800)
+
 
 
 except Exception as e:
